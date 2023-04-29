d5fdb9542dc621c1b273fce7a422c1156d21fb53
sarthakpunjabi <spunjabi2807.ca18@chitkara.edu.in>
2023-04-29 11:18:44 +0100

    Adding the backend to frontend

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -22,7 +22,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://image-generator-wg93.onrender.com/api/v1/post', {
           method:'POST',
           headers: {
             'Content-Type':'application/json',
@@ -61,7 +61,7 @@ const CreatePost = () => {
       
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle/',{
+        const response = await fetch('https://image-generator-wg93.onrender.com/api/v1/dalle/',{
           method:'POST',
           headers:{
             'Content-Type':'application/json',
